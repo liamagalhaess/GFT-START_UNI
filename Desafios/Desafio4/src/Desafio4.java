ea826899599b01c02b69fd25a3c6fd0ab983995c
Lia Daiely Magalhães <48828699+liamagalhaess@users.noreply.github.com>
2021-03-05 16:07:18 -0300

    Update Desafio4.java
    Atualização

--- Desafios/Desafio4/src/Desafio4.java
+++ Desafios/Desafio4/src/Desafio4.java
@@ -1,3 +1,24 @@
+import java.util.Scanner;
+
 public class Desafio4 {
 
-}
+
+    public static void main(String[] args){
+
+        int total = 0;
+        int valor;
+        Scanner scanner = new Scanner(System.in);
+
+        while(total < 157){
+            System.out.println("Insira um valor: ");
+            valor = scanner.nextInt();
+
+            if(valor < 0) {
+                System.out.println("Números negativos não são permitidos");
+            }else{
+                total += valor;
+            }
+        }
+        System.out.println("TOTAL: " + total);
+
+    }
